a79aa2fd45ef98432f3fea3648e4d7868618ec67
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-03-31 00:27:51 +0500

    next 13 project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+      <h1 className="text-3xl mt-2 text-black font-bold mb-5">
+        Welcome to the Amazon Web Scraper
+      </h1>
+      <h2 className="text-lg italic text-center text-black/50">
+        To learn more about the Amazon Web Scraper, contact me at
+        <Link
+          href="mailto:hussain2000.rizvi@gmail.com"
+          target="_blank"
+          className="text-blue-600 /50"
+        >
+          {" "}
+          hussain2000.rizvi@gmail.com
+        </Link>
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
